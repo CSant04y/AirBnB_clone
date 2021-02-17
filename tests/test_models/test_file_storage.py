b65646b52f2c14a0b686ad56f20c77152c9c8a94
CSant04y <esquivelcarlos27893@gmail.com>
2021-02-17 22:44:36 +0000

    added some changes

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This is the Unitest for file_storage.py"""
+
+
+from models.engine import FileStorage
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+def TestFileStorage(unittest.TestCase):
+    """This is Unittest for the method FileStorage"""
+
+    def setUp(self):
+        """This is the setup method for Unittesting"""
+
+    def test_all(self):
+        """This is the Unittest for the method all"""
+
+    def test_new(self, obj):
+        """This is a Unittest for the method new"""
+
+    def test_save(self):
+        """This is a Unittest for the the method save"""
+
+    def test_reload(self):
+        """This is a Unittest for the method reload"""
